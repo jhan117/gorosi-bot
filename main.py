80d5fb4ba9fefdffcc73c35cf038a987888daeeb
2001Kaye <jhan117@naver.com>
2022-05-21 17:39:17 +0900

    add weather and loop tasks

--- main.py
+++ main.py
@@ -1,82 +1,11 @@
-import requests
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import os
-from bs4 import BeautifulSoup
+from datetime import datetime
+import asyncio
 
-
-# gosi url
-gosi_url = "https://www.gosi.kr/uat/uia/gosiMain.do"
-
-
-# embed maker
-def make_embed(newsList):
-    embed = discord.Embed(
-        title='사이버국가고시센터', url=gosi_url, color=0xFF0000)
-    for value in newsList.values():
-        embed.add_field(name=value[1], value=value[0], inline=False)
-    return embed
-
-
-# 각 리스트
-def get_news_ul(id):
-    html = requests.get(gosi_url)
-    soup = BeautifulSoup(html.content, "html.parser")
-
-    return soup.find(id=f"tab-content{id}")
-
-
-# 공지사항
-def get_news_notice():
-    newsList = {}
-
-    ul = get_news_ul(1)
-
-    infos = ul.select('b > a')
-    dates = ul.find_all(class_='date')
-
-    for i in range(len(infos)):
-        newsList[i] = [dates[i].text]
-        newsList[i].append(infos[i].text)
-    return newsList
-
-
-# 새글
-def get_new_news():
-    newsList = {}
-
-    ul = get_news_ul(1)
-
-    dates = ul.find_all(class_='date')
-
-    newImg = ul.find_all('img')
-    if newImg:
-        for i in range(len(newImg)):
-            infos = newImg[i].parent.find('a')
-            newsList[i] = [dates[i].text]
-            newsList[i].append(infos.text)
-
-    return newsList
-
-
-# 공채 7급 소식
-def get_news7(limit):
-    try:
-        newsList = {}
-
-        ul = get_news_ul(3)
-
-        infos = ul.find_all('a')
-        dates = ul.find_all(class_='date')
-
-        for i in range(int(limit)):
-            newsList[i] = [dates[i].text]
-            newsList[i].append(infos[i].text)
-
-        return newsList
-    except:
-        return None
-
+from gosi_fuc import *
+from weather_fuc import *
 
 bot = commands.Bot(command_prefix='!',
                    activity=discord.Game(name="시켜서"))
@@ -85,17 +14,41 @@ bot = commands.Bot(command_prefix='!',
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+    test.start()
+
+    while True:
+        try:
+            now_time = convert_int_time(datetime.now())
+
+            if forecast.is_running() == True:
+                if now_time > 123000:
+                    forecast.stop()
+            else:
+                if 115000 <= now_time <= 123000:
+                    forecast.start()
+            await asyncio.sleep(1)
+        except:
+            print("on_ready has error")
+
+
+# 새글 3시간마다 알림
+@tasks.loop(seconds=1)
+async def test():
+    if datetime.now().time().isoformat(timespec='seconds') in time_list:
+        get_new_post = get_new()
+
+        if get_new_post:
+            new_embed = make_embed(get_new_post)
+            await bot.get_channel(gosi_channel).send(embed=new_embed)
 
 
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
-        message = '? 똑바로 쓴 거 맞아요?'
-    else:
-        message = '오류 떴네요 근데 뭔 오류인지 모르겠어요 ㅋㅋ ㅎㅎ ㅈㅅ'
+        error_message = '? 똑바로 쓴 거 맞아요?'
 
-    await ctx.send(message, delete_after=3)
-    await ctx.message.delete(delay=3)
+        await ctx.send(error_message, delete_after=3)
+        await ctx.message.delete(delay=3)
 
 
 @bot.command(name='청소')
@@ -145,6 +98,37 @@ async def news7(ctx, limit):
     else:
         await ctx.channel.send("채널 이름도 읽지 않는 사람에겐 알려주기 싫어요", delete_after=3)
         await ctx.message.delete(delay=3)
+
+
+# === 기본 날씨 정보 ===
+@bot.command(name='날씨')
+async def weather(ctx):
+    if str(ctx.channel) == "weather":
+        await ctx.channel.send(embed=make_weather_embed())
+
+
+# === 산책 날씨 정보 ===
+@bot.command(name='산책')
+async def walk_a_dog(ctx):
+    if str(ctx.channel) == "weather":
+        weather_result = WalkADog().check_weather()
+
+        if not weather_result:
+            await ctx.channel.send(WalkADog().check_clothes())
+        else:
+            await ctx.channel.send(weather_result)
+
+        await ctx.channel.send(WalkADog().check_dust())
+        await ctx.channel.send(WalkADog().check_wind())
+
+
+# === 10분 간격으로 재 요청 30분까지 ===
+@tasks.loop(seconds=1)
+async def forecast():
+    if 115000 <= convert_int_time(datetime.now()) <= 163000:
+        forecast_message = get_forecast()
+        if forecast_message:
+            await bot.get_channel(walk_channel).send(forecast_message)
 
 
 @news7.error
